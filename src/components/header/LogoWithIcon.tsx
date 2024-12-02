a1e28313bc7bf59e70d122f1b8b6e87264f248e3
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-12-02 14:58:40 +0000

    create logo component

---
+++ src/components/header/LogoWithIcon.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router";
+import { Title } from "@mantine/core";
+import styles from "./header.module.css";
+import Logo from "../../../public/svg/logo.svg?react";
+
+function LogoWithIcon() {
+  return (
+    <div className={styles.logoContainer}>
+      <Link to="/">
+        <Title order={1}>popstats</Title>
+      </Link>
+      <Logo height={30} width={30} />
+    </div>
+  );
+}
+
+export default LogoWithIcon;
