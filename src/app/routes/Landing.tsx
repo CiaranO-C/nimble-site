5da29ada53d9b3b933766c5999a7d4bffb6e0c30
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-12-02 14:06:16 +0000

    write landing page content

--- src/app/routes/Landing.tsx
+++ src/app/routes/Landing.tsx
@@ -1,25 +1,62 @@
-import styled from "styled-components";
-import "../App.css";
 import { Link } from "react-router";
+import { em, Group, Text, Title, useMantineColorScheme } from "@mantine/core";
+import styles from "./styles/landing.module.css";
+import Logo from "../../../public/svg/logo.svg?react";
+import { modals } from "@mantine/modals";
+import { useMediaQuery } from "@mantine/hooks";
+
+const { container, containerDark, title, content, demoLink, logo, aboutText } =
+  styles;
 
 function Landing() {
+  const { colorScheme: theme } = useMantineColorScheme();
+  const isMobile = useMediaQuery("(max-width: 50em)");
+
   return (
-    <Main>
-      <Title>popstats</Title>
-      <Link to="/demo">Demo</Link>
-    </Main>
+    <main className={`${container} ${theme === "dark" ? containerDark : ""}`}>
+      <div className={content}>
+        <Title data-text="popstats" className={title} size={em(200)} order={1}>
+          popstats
+        </Title>
+        <Text
+          w="700px"
+          pb="20px"
+          mt="30px"
+          mb="50px"
+          size="xl"
+          lh="1.5"
+          flex="1"
+          className={aboutText}
+        >
+          Welcome to Popstats, a platform designed for Depop sellers.
+          <br />
+          Effortlessly upload your sales data and unlock valuable insights into
+          your store's performance. Track trends, optimize pricing, and make
+          data driven decisions. Taking your store to the next level has never
+          been easier, start using Popstats today!
+        </Text>
+        <Group>
+          <button
+            onClick={() =>
+              modals.open({
+                title: "Sign up",
+                fullScreen: isMobile,
+                centered: true,
+                children: <p>coming soon</p>,
+              })
+            }
+            className={demoLink}
+          >
+            Sign up
+          </button>
+          <Link className={demoLink} to="/demo">
+            Explore demo
+          </Link>
+        </Group>
+      </div>
+      <Logo color="red" width="700" height="700" className={logo} />
+    </main>
   );
 }
 
 export default Landing;
-
-const Main = styled.main`
-  height: 100vh;
-  width: 100vw;
-  background-color: red;
-`;
-
-const Title = styled.h1`
-  font-family: "Trap";
-  font-size: 12rem;
-`;
