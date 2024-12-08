0114c3047fb0f88930fc777f75f1a8d5adf8f7b3
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-12-08 12:00:27 +0000

    update imports to assets dir

--- src/components/header/LogoWithIcon.tsx
+++ src/components/header/LogoWithIcon.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router";
 import { Title } from "@mantine/core";
 import styles from "./header.module.css";
-import Logo from "../../../public/svg/logo.svg?react";
+import Logo from "../../assets/svg/popstatsLogo.svg?react"
 
 function LogoWithIcon() {
   return (
