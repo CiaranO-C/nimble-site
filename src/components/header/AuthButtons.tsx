4819a68a8ef71bca7fb7a974f76a5ef0f1903917
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-12-08 12:02:47 +0000

    simplify component

--- src/components/header/AuthButtons.tsx
+++ src/components/header/AuthButtons.tsx
@@ -1,46 +1,20 @@
-import { Button } from "@mantine/core";
-import { useMediaQuery } from "@mantine/hooks";
-import { modals } from "@mantine/modals";
 import styles from "./header.module.css";
+import AuthButton from "../auth/AuthButton";
 
 function AuthButtons() {
-  const isMobile = useMediaQuery("(max-width: 50em)");
-
   return (
     <>
       <div className={styles.authButtons}>
-        <Button
-          onClick={() => {
-            modals.open({
-              title: "Sign in",
-              fullScreen: isMobile,
-              centered: true,
-              children: (
-                <>
-                  <p>This feature is coming soon!</p>
-                </>
-              ),
-            });
-          }}
-        >
-          Sign in
-        </Button>
-        <Button
-          onClick={() => {
-            modals.open({
-              title: "Join popstats",
-              fullScreen: isMobile,
-              centered: true,
-              children: (
-                <>
-                  <p>This feature is coming soon!</p>
-                </>
-              ),
-            });
-          }}
-        >
-          Sign up
-        </Button>
+        <AuthButton
+          title="Sign in"
+          modalTitle="Welcome back"
+          children={<p>This feature is coming soon!</p>}
+        />
+        <AuthButton
+          title="Join now"
+          modalTitle="Join Popstats"
+          children={<p>This feature is coming soon!</p>}
+        />
       </div>
     </>
   );
