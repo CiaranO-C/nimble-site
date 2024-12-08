289c30d0b9ea970ded484e40aaf411aa2148b73f
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-12-08 10:41:58 +0000

    create public layout component

---
+++ src/app/routes/layouts/PublicLayout.tsx
@@ -0,0 +1,34 @@
+import { AppShell, Group, rem } from "@mantine/core";
+import { useHeadroom } from "@mantine/hooks";
+import { Outlet } from "react-router";
+import LogoWithIcon from "../../../components/header/LogoWithIcon";
+import AuthButtons from "../../../components/header/AuthButtons";
+import ThemeSwitch from "../../../components/header/ThemeSwitch";
+
+function PublicLayout() {
+  const pinned = useHeadroom({ fixedAt: 120 });
+
+  return (
+    <>
+      <AppShell
+        header={{ height: 60, collapsed: !pinned, offset: false }}
+        padding="50px 50px 0px"
+        pos="relative"
+        style={{ overflow: "hidden" }}
+      >
+        <AppShell.Header>
+          <Group h="100%" px="md">
+            <LogoWithIcon />
+            <ThemeSwitch />
+            <AuthButtons />
+          </Group>
+        </AppShell.Header>
+        <AppShell.Main pt={`calc(${rem(60)} + var(--mantine-spacing-md))`}>
+          <Outlet />
+        </AppShell.Main>
+      </AppShell>
+    </>
+  );
+}
+
+export default PublicLayout;
