cdf7db352dd69c5a3afaecc611b034c632580ffc
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-12-19 23:03:00 +0000

    use grid layout components, render bar chart and info cards

--- src/app/routes/PreviewStats.tsx
+++ src/app/routes/PreviewStats.tsx
@@ -1,22 +1,64 @@
-import { Button, Title } from "@mantine/core";
+import { Button, Grid, Group, Paper, Stack, Text, Title } from "@mantine/core";
 import { getTestData } from "../../features/demo/utils";
 import { useState } from "react";
 import CustomAreaChart from "../../components/charts/CustomAreaChart";
+import TotalCard from "../../components/stats/TotalCard";
+import { AverageSales, SaleByDate } from "../../components/charts/type";
+import CustomBarChart from "../../components/charts/CustomBarChart";
+
+interface ChartData {
+  totalSales: Record<string, number>; 
+  salesByDate: SaleByDate[]; 
+  averageSales: AverageSales;
+}
+
 function PreviewStats() {
-  const [areaData, setAreaData] = useState<any | null>(null);
+  const [chartData, setChartData] = useState<ChartData | null>(null);
 
   return (
     <>
-      <Title>Stats!</Title>
-      <Button
-        onClick={async () => {
-          const data = await getTestData();
-          setAreaData(data);
-        }}
-      >
-        hello
-      </Button>
-      {areaData && <CustomAreaChart data={areaData} />}
+      <Grid>
+        <Grid.Col span={12}>
+          <Stack>
+            <Title>Stats!</Title>
+            <Button
+              onClick={async () => {
+                const data = await getTestData();
+                setChartData(data);
+              }}
+            >
+              load data
+            </Button>
+          </Stack>
+        </Grid.Col>
+
+        {chartData && (
+          <>
+            <Grid.Col span={12}>
+              <Group>
+                {Object.entries(chartData.totalSales).map(
+                  ([key, value]: [string, number]) => (
+                    <TotalCard key={key} title={key} value={value} />
+                  ),
+                )}
+              </Group>
+            </Grid.Col>
+            <Grid.Col span={12}>
+              <CustomAreaChart data={chartData.salesByDate} />
+            </Grid.Col>
+            <Grid.Col span={6}>
+              <CustomBarChart data={chartData.averageSales.byDate.monthly} />
+            </Grid.Col>
+            <Grid.Col span={6}>
+              <Stack>
+                <Paper>
+
+                </Paper>
+              </Stack>
+            </Grid.Col>
+          </>
+        )}
+      </Grid>
     </>
   );
 }
