7fb0300de5586c2a365fd2866f69bcd4ba343414
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-12-24 17:57:53 +0000

    update component to use dateType value instead of toggling monthly boolean

--- src/components/charts/DateTooltip.tsx
+++ src/components/charts/DateTooltip.tsx
@@ -1,24 +1,35 @@
 import { getFilteredChartTooltipPayload } from "@mantine/charts";
 import { Paper, Text } from "@mantine/core";
+import { dayMap, isKeyOfDayMap, isKeyOfMonthMap, monthMap } from "./utils";
+import { TDateISO } from "./type";
 
 interface ChartTooltipProps {
   label: string;
   payload: Record<string, any>[] | undefined;
-  monthly?: boolean;
+  dateType?: DateType;
 }
 
-function DateTooltip({ payload, monthly = false }: ChartTooltipProps) {
+type DateType = "default" | "daily" | "monthly";
+
+type DateValue = TDateISO | keyof typeof monthMap | keyof typeof dayMap;
+
+function generateDateString(dateValue: DateValue, dateType: DateType) {
+  if (dateType === "daily" && isKeyOfDayMap(dateValue))
+    return dayMap[dateValue];
+  if (dateType === "monthly" && isKeyOfMonthMap(dateValue))
+    return monthMap[dateValue];
+
+  return new Date(dateValue).toDateString();
+}
+
+function DateTooltip({ payload, dateType = "default" }: ChartTooltipProps) {
   if (!payload) return;
-  
-  const iso = getFilteredChartTooltipPayload(payload)[0]?.payload?.date;
-  const unfilter = payload;
-  const filteredPayload = getFilteredChartTooltipPayload(payload)[0]
-  console.log(unfilter);
-  console.log(filteredPayload);
-  
-  
-  //const date = monthly ? new Date(new Date().setMonth(payload)) : new Date(iso).toDateString();
-let date = 'test';
+
+  const filtered = getFilteredChartTooltipPayload(payload)[0];
+  const dateValue: DateValue = filtered?.payload?.date;
+
+  const date = generateDateString(dateValue, dateType);
+
   return (
     <Paper px="md" py="sm" withBorder shadow="md" radius="xl">
       <Text fw={500} mb={5}>
