3fee93841018c0fcc4b01aa54186747566cffbae
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-12-24 17:57:15 +0000

    update bar chart to correctly display day of week or month along x axis

--- src/components/charts/CustomBarChart.tsx
+++ src/components/charts/CustomBarChart.tsx
@@ -1,18 +1,40 @@
 import { BarChart } from "@mantine/charts";
-import { generateSeries } from "./utils";
-import { useMantineTheme } from "@mantine/core";
+import { dayMap, generateSeries, isKeyOfDayMap } from "./utils";
+import { Button, useMantineTheme } from "@mantine/core";
+import DateTooltip from "./DateTooltip";
+import { useToggle } from "@mantine/hooks";
 
 function CustomBarChart({ data }) {
+  const [daily, toggle] = useToggle();
+  const currentData = daily ? data.daily : data.monthly;
   const colors = useMantineTheme().colors;
+
+  console.log(data);
 
   return (
     <>
+      <Button onClick={() => toggle()}>toggle</Button>
       <BarChart
-        h={500}
-        data={data}
+        h={300}
+        data={currentData}
         dataKey="date"
-        series={generateSeries(data, ["date", "month"], colors)}
-        barProps={{ radius: 10 }}
+        series={generateSeries(currentData, ["date"], colors)}
+        barProps={{ radius: 10, maxBarSize: 80, isAnimationActive: true }}
+        xAxisProps={{
+          tickFormatter: (tick) => {
+            if (daily && isKeyOfDayMap(tick)) return dayMap[tick];
+
+            const month = new Date(new Date().setMonth(tick))
+              .toString()
+              .split(" ")[1];
+            return month;
+          },
+        }}
+        tooltipProps={{
+          content: ({ label, payload }) => (
+            <DateTooltip label={label} payload={payload} dateType={daily ? "daily" : "monthly"} />
+          ),
+        }}
       />
     </>
   );
