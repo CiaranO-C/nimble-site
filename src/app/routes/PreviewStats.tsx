26b1a27504db000cb7f7111620f9a6544345f140
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2025-01-08 22:59:51 +0000

    update chartData type, split chart props into three, scaffold pie charts

--- src/app/routes/PreviewStats.tsx
+++ src/app/routes/PreviewStats.tsx
@@ -1,19 +1,53 @@
-import { Button, Grid, Group, Paper, Stack, Text, Title } from "@mantine/core";
+import { Button, Grid, Group, Paper, Stack, Title } from "@mantine/core";
 import { getTestData } from "../../features/demo/utils";
 import { useState } from "react";
 import CustomAreaChart from "../../components/charts/CustomAreaChart";
-import TotalCard from "../../components/stats/TotalCard";
-import { AverageSales, SaleByDate } from "../../components/charts/type";
+
+import {
+  AverageSales,
+  RevenueByDate,
+  SaleItemsByDate,
+  SalesByDate,
+} from "../../components/charts/type";
 import CustomBarChart from "../../components/charts/CustomBarChart";
+import { PieChart, PieChartCell } from "@mantine/charts";
 
 interface ChartData {
-  totalSales: Record<string, number>; 
-  salesByDate: SaleByDate[]; 
-  averageSales: AverageSales;
+  buyers: {
+    all: {};
+    repeat: {};
+  };
+  sales: {
+    revenue: {
+      total: number;
+      byDate: { all: RevenueByDate[] };
+      average: AverageSales;
+    };
+    count: {
+      byDate: { all: SalesByDate[]; items: SaleItemsByDate[] };
+    };
+  };
 }
 
 function PreviewStats() {
   const [chartData, setChartData] = useState<ChartData | null>(null);
+
+  function pieChartData() {
+    const { all, repeat } = chartData.buyers;
+    return [
+      { name: "first time", value: all.count - repeat.count, color: "red" },
+      { name: "repeat", value: repeat.count, color: "blue" },
+    ];
+  }
+
+  function pieChartDataPct() {
+    const { all, repeat } = chartData.buyers;
+    const pct = (val: number) => parseFloat(((val / 55) * 100).toFixed(1));
+    return [
+      { name: "all", value: pct(all.count - repeat.count), color: "red" },
+      { name: "repeat", value: pct(repeat.count), color: "blue" },
+    ];
+  }
 
   return (
     <>
@@ -35,26 +69,48 @@ function PreviewStats() {
         {chartData && (
           <>
             <Grid.Col span={12}>
-              <Group>
-                {Object.entries(chartData.totalSales).map(
-                  ([key, value]: [string, number]) => (
-                    <TotalCard key={key} title={key} value={value} />
-                  ),
-                )}
-              </Group>
+              <Group></Group>
             </Grid.Col>
             <Grid.Col span={12}>
-              <CustomAreaChart data={chartData.salesByDate} />
+              <CustomAreaChart
+                revenue={chartData.sales.revenue.byDate.all}
+                sales={chartData.sales.count.byDate.all}
+                items={chartData.sales.count.byDate.items}
+              />
             </Grid.Col>
             <Grid.Col span={6}>
-              <CustomBarChart data={chartData.averageSales.byDate.monthly} />
+              <CustomBarChart
+                hourly={chartData.sales.revenue.average.byTime}
+                daily={chartData.sales.revenue.average.byDate.daily}
+                monthly={chartData.sales.revenue.average.byDate.monthly}
+              />
             </Grid.Col>
             <Grid.Col span={6}>
-              <Stack>
-                <Paper>
-
+              <Paper shadow="xs" p="xl">
+                <Stack>
+                  <Title order={3}>Insights</Title>
+                </Stack>
+              </Paper>
+            </Grid.Col>
+            <Grid.Col span={6}>
+              <Group>
+                <PieChart data={pieChartData()} withTooltip />
+                <Paper shadow="xs" p="xl">
+                  <Stack>
+                    <Title order={3}>Insights</Title>
+                  </Stack>
                 </Paper>
-              </Stack>
+              </Group>
+            </Grid.Col>
+            <Grid.Col span={6}>
+              <Group>
+                <PieChart data={pieChartDataPct()} withTooltip/>
+                <Paper shadow="xs" p="xl">
+                  <Stack>
+                    <Title order={3}>Insights</Title>
+                  </Stack>
+                </Paper>
+              </Group>
             </Grid.Col>
           </>
         )}
