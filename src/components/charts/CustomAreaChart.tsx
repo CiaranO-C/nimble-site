04b3e92fac44de61f6aed763403521091bc0da16
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2025-01-08 12:49:52 +0000

    add checkboxes for user chart options, update props to 3 datasources instead of 1

--- src/components/charts/CustomAreaChart.tsx
+++ src/components/charts/CustomAreaChart.tsx
@@ -3,34 +3,93 @@ import {
   generateSeries,
   generateTicks,
   getDateRange,
-  getMonth,
   leadingZero,
 } from "./utils";
-import CustomTooltip from "./CustomTooltip";
-import { useToggle } from "@mantine/hooks";
-import { Button, useMantineTheme } from "@mantine/core";
+import DateTooltip from "./DateTooltip";
+import { Checkbox, SegmentedControl, useMantineTheme } from "@mantine/core";
+import { useState } from "react";
+import {
+  RevenueData,
+  RevenueByDate,
+  SalesByDate,
+  SaleItemsByDate,
+  SaleData,
+  ItemData,
+} from "./type";
 
-function CustomAreaChart({ data }) {
-  const [daily, toggle] = useToggle();
+function CustomAreaChart(props: {
+  revenue: RevenueByDate[];
+  sales: SalesByDate[];
+  items: SaleItemsByDate[];
+}) {
+  const [segmentValue, setSegmentValue] = useState<ChartDataType>("revenue");
+  const [series, setSeries] = useState<{
+    revenue: RevenueData[];
+    sales: SaleData[];
+    items: ItemData[];
+  }>({
+    revenue: ["total"],
+    sales: ["sales"],
+    items: ["items"],
+  });
   const colors = useMantineTheme().colors;
+
+  //values must correspond to prop names
+  const data = props[segmentValue];
+
+  const daily = data.length <= 30;
+  type ChartDataType = "revenue" | "sales" | "items";
+
+  function handleSegment(value: string) {
+    setSegmentValue(value as ChartDataType);
+  }
 
   return (
     <>
-      <Button onClick={() => toggle()}>toggle</Button>
+      <SegmentedControl
+        value={segmentValue}
+        onChange={handleSegment}
+        data={[
+          { label: "Revenue", value: "revenue" },
+          { label: "Sales", value: "sales" },
+          { label: "Items", value: "items" },
+        ]}
+      />
+      <Checkbox.Group
+        value={series[segmentValue]}
+        onChange={(values: string[]) => {
+          const current = segmentValue;
+          if (values.length === 0) return;
+          setSeries((s) => ({ ...s, [current]: values }));
+        }}
+      >
+        {segmentValue === "revenue" ? (
+          <>
+            <Checkbox value="total" label="Gross Revenue" />
+            <Checkbox value="net" label="Net Revenue" />
+            <Checkbox value="shipping" label="Shipping" />
+          </>
+        ) : segmentValue === "sales" ? (
+          <>
+            <Checkbox value="sales" label="Sales" />
+            <Checkbox value="refunds" label="Refunds" />
+            <Checkbox value="bundles" label="Bundles" />
+          </>
+        ) : (
+          <>
+            <Checkbox value="items" label="Items" />
+            <Checkbox value="boosted" label="Boosted" />
+          </>
+        )}
+      </Checkbox.Group>
+
       <AreaChart
         h={400}
-        data={
-          daily
-            ? data.slice(data.length - data.length - 30)
-            : data.map((data) => {
-                return {
-                  ...data,
-                  month: getMonth(data.date),
-                };
-              })
-        }
-        dataKey={daily ? "date" : "month"}
-        areaProps={{ isAnimationActive: true }}
+        unit={segmentValue === "revenue" ? "£" : ""}
+        // referenceLines={[{ y: 10, label: "Average sale", color: "white"}]}
+        data={data}
+        dataKey={"date" /*daily ? "date" : "month"*/}
+        // areaProps={{ isAnimationActive: true, animateNewValues: true }}
         tooltipAnimationDuration={200}
         fillOpacity={0.5}
         gridAxis={daily ? "x" : "xy"}
@@ -40,23 +99,26 @@ function CustomAreaChart({ data }) {
         xAxisProps={{
           ticks: daily ? undefined : generateTicks(data),
           tickFormatter: (tick: string, i): string => {
+            const date = new Date(tick);
+            const day = date.getDate();
+            const month = date.getMonth() + 1;
             if (daily) {
               if ((i + 1) % 2 === 0) return "";
-              const date = new Date(tick);
-              const day = date.getDate();
-              const month = date.getMonth() + 1;
               return `${leadingZero(day)}/${leadingZero(month)}`;
             }
             return tick;
           },
+          axisLine: true,
         }}
+        yAxisProps={{ axisLine: true }}
         tooltipProps={{
           content: ({ label, payload }) => (
-            <CustomTooltip label={label} payload={payload} />
+            <DateTooltip label={label} payload={payload} />
           ),
         }}
-        textColor="white"
-        series={generateSeries(data, ["date", "month"], colors)}
+        series={generateSeries(data[0], series[segmentValue], colors)}
+        withDots={false}
+        areaProps={{ isAnimationActive: true, animateNewValues: false }}
       />
     </>
   );
