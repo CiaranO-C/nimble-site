3cbef0e3e773d8cbade04b66bd9f0d3fa142ac50
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2025-01-08 23:02:31 +0000

    remove toggle, implement radio button controls

--- src/components/charts/CustomBarChart.tsx
+++ src/components/charts/CustomBarChart.tsx
@@ -1,8 +1,21 @@
 import { BarChart } from "@mantine/charts";
-import { dayMap, generateSeries, isKeyOfDayMap, monthMap } from "./utils";
-import { Button, useMantineTheme } from "@mantine/core";
+import {
+  dayMap,
+  generateSeries,
+  isKeyOfDayMap,
+  isKeyOfMonthMap,
+  monthMap,
+} from "./utils";
+import { Button, Group, Radio, useMantineTheme } from "@mantine/core";
 import DateTooltip from "./DateTooltip";
 import { useToggle } from "@mantine/hooks";
+import { useState } from "react";
+import {
+  AverageSales,
+  DailyAverage,
+  HourlyAverages,
+  MonthlyAverage,
+} from "./type";
 
 interface AverageDailyType {
   average: number;
@@ -19,28 +32,51 @@ function sortDaily(daily: AverageDailyType[]) {
   return daily;
 }
 
-function CustomBarChart({ data }) {
-  const [daily, toggle] = useToggle();
-  const currentData = daily ? sortDaily(data.daily) : data.monthly;
+type DateType = "monthly" | "daily" | "hourly";
+
+function CustomBarChart(props: {
+  daily: DailyAverage[];
+  monthly: MonthlyAverage[];
+  hourly: HourlyAverages[];
+}) {
+  const [radioValue, setRadioValue] = useState<DateType>("monthly");
+  const currentData =
+    radioValue === "daily" ? sortDaily(props[radioValue]) : props[radioValue];
   const colors = useMantineTheme().colors;
+console.log(currentData);
+const series = generateSeries(currentData, ["average"], colors);
+console.log(series);
+
+  function handleRadio(value: string) {
+    setRadioValue(value as DateType);
+  }
 
   return (
     <>
-      <Button onClick={() => toggle()}>toggle</Button>
+      <Radio.Group value={radioValue} onChange={handleRadio}>
+        <Group>
+          <Radio value="monthly" label="Monthly" />
+          <Radio value="daily" label="Daily" />
+          <Radio value="hourly" label="Hourly" />
+        </Group>
+      </Radio.Group>
       <BarChart
         h={300}
         data={currentData}
-        dataKey="date"
-        series={generateSeries(currentData, ["date"], colors)}
+        dataKey={radioValue === "hourly" ? "hour" : "date"}
+        series={generateSeries(currentData[0], ["average"], colors)}
         barProps={{ radius: 10, maxBarSize: 80, isAnimationActive: true }}
         xAxisProps={{
-          tickFormatter: (tick) => {
-            if (daily && isKeyOfDayMap(tick)) return dayMap[tick];
-
-            const month = new Date(new Date().setMonth(tick))
-              .toString()
-              .split(" ")[1];
-            return month;
+          tickFormatter: (tick, i) => {
+            if (radioValue === "daily" && isKeyOfDayMap(tick)) {
+              const day = dayMap[tick];
+              const chars = day === "Tuesday" || day === "Thursday" ? -3 : 3;
+              return day.slice(0, chars);
+            } else if (radioValue === "monthly" && isKeyOfMonthMap(tick)) {
+              return monthMap[tick].slice(0, 3);
+            }
+            if (i % 3 !== 0) return "";
+            return tick;
           },
         }}
         tooltipProps={{
@@ -48,7 +84,7 @@ function CustomBarChart({ data }) {
             <DateTooltip
               label={label}
               payload={payload}
-              dateType={daily ? "daily" : "monthly"}
+              dateType={radioValue === "daily" ? "daily" : "monthly"}
             />
           ),
         }}
