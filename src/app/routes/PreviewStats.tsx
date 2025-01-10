18717d492d046d5a0e3090f9e1f03cd651d4789e
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2025-01-10 20:25:41 +0000

    move pie charts, update chart data type

--- src/app/routes/PreviewStats.tsx
+++ src/app/routes/PreviewStats.tsx
@@ -4,50 +4,23 @@ import { useState } from "react";
 import CustomAreaChart from "../../components/charts/CustomAreaChart";
 
 import {
-  AverageSales,
-  RevenueByDate,
-  SaleItemsByDate,
-  SalesByDate,
+  BuyersData,
+  SalesCountData,
+  SalesRevenueData,
 } from "../../components/charts/type";
 import CustomBarChart from "../../components/charts/CustomBarChart";
-import { PieChart, PieChartCell } from "@mantine/charts";
+import CustomPieChart from "../../components/charts/CustomPieChart";
 
 interface ChartData {
-  buyers: {
-    all: {};
-    repeat: {};
-  };
+  buyers: BuyersData;
   sales: {
-    revenue: {
-      total: number;
-      byDate: { all: RevenueByDate[] };
-      average: AverageSales;
-    };
-    count: {
-      byDate: { all: SalesByDate[]; items: SaleItemsByDate[] };
-    };
+    revenue: SalesRevenueData;
+    count: SalesCountData;
   };
 }
 
 function PreviewStats() {
   const [chartData, setChartData] = useState<ChartData | null>(null);
-
-  function pieChartData() {
-    const { all, repeat } = chartData.buyers;
-    return [
-      { name: "first time", value: all.count - repeat.count, color: "red" },
-      { name: "repeat", value: repeat.count, color: "blue" },
-    ];
-  }
-
-  function pieChartDataPct() {
-    const { all, repeat } = chartData.buyers;
-    const pct = (val: number) => parseFloat(((val / 55) * 100).toFixed(1));
-    return [
-      { name: "all", value: pct(all.count - repeat.count), color: "red" },
-      { name: "repeat", value: pct(repeat.count), color: "blue" },
-    ];
-  }
 
   return (
     <>
@@ -94,7 +67,10 @@ function PreviewStats() {
             </Grid.Col>
             <Grid.Col span={6}>
               <Group>
-                <PieChart data={pieChartData()} withTooltip />
+                <CustomPieChart
+                  data={chartData.buyers.byCountry}
+                  initialBool={false}
+                />
                 <Paper shadow="xs" p="xl">
                   <Stack>
                     <Title order={3}>Insights</Title>
@@ -104,7 +80,13 @@ function PreviewStats() {
             </Grid.Col>
             <Grid.Col span={6}>
               <Group>
-                <PieChart data={pieChartDataPct()} withTooltip/>
+                <CustomPieChart
+                  data={{
+                    all: chartData.buyers.all,
+                    repeat: chartData.buyers.repeat,
+                  }}
+                  initialBool={true}
+                />
                 <Paper shadow="xs" p="xl">
                   <Stack>
                     <Title order={3}>Insights</Title>
