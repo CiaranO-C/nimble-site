ec4f85fba26ef3566107281a17d0996ecfaac3dc
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2025-01-17 16:38:32 +0000

    allow total card to render list instead of single value

--- src/components/stats/TotalCard.tsx
+++ src/components/stats/TotalCard.tsx
@@ -1,13 +1,44 @@
-import { Paper, Stack, Text, Title } from "@mantine/core";
+import { Stack, Text, Title } from "@mantine/core";
 
-function TotalCard({ title, value }: { title: string; value: number }) {
+function TotalCard({
+  title,
+  values,
+  money,
+}: {
+  title: string;
+  values: { name: string; value: number }[];
+  money?: string;
+}) {
   return (
-    <Paper>
-      <Stack>
-        <Title order={3}>Total {`${title.charAt(0).toUpperCase()}${title.slice(1)}`}</Title>
-        <Text>{value}</Text>
+    <Stack
+      style={{
+        width: "100%",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <Title
+        order={3}
+        style={{ textAlign: "center" }}
+      >{`${title.charAt(0).toUpperCase()}${title.slice(1)}`}</Title>
+      <Stack style={{ maxWidth: "180px", width: "100%" }}>
+        {values.map(({ name, value }) => (
+          <Text
+            key={name} 
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              width: "100%",
+            }}
+          >
+            <span style={{ flex: 1, textAlign: "left" }}>{name}</span>
+            <span style={{ flex: 1, textAlign: "right" }}>
+              {money ? `${money}${value}` : value}
+            </span>
+          </Text>
+        ))}
       </Stack>
-    </Paper>
+    </Stack>
   );
 }
 
